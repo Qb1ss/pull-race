9d11ece283f9db78e0337e21636e8a50f5298905
Gleb B <gleb.bortsov79@gmail.com>
2022-06-04 14:18:28 +1000

    Работа:
    * интеграция Facebook SDK

---
+++ Assets/Scripts/Analytics/Analytics_Facebook.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Facebook.Unity;
+
+public class Analytics_Facebook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
