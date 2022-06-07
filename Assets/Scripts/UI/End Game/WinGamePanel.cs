a5be024211a5468f2f13b0ed41c0a22eee378e32
Gleb B <gleb.bortsov79@gmail.com>
2022-06-07 15:48:18 +1000

    Добавил:
    * красивое получение монет

--- Assets/Scripts/UI/End Game/WinGamePanel.cs
+++ Assets/Scripts/UI/End Game/WinGamePanel.cs
@@ -37,6 +37,16 @@ namespace Interface.EndGame
         private int _coinDivision = 10;
         private int _coinsValue;
 
+        [Header("Coin Animation")]
+        [SerializeField] private RectTransform _coinRectTransform;
+        [Space(height: 5f)]
+
+        [SerializeField] private float _timePlayingAnimation = 2f;
+        [Space(height: 5f)]
+
+        [SerializeField] private Vector2 _startPosition;
+        private Vector2 _targetPosition;
+
         private SceneTrancition _sceneTrancition;
 
 
@@ -49,6 +59,10 @@ namespace Interface.EndGame
             _nextLevelButton.onClick.AddListener(() => TrancitionToNextLevel());
 
             UpdateStartVisual();
+
+            _targetPosition = _coinRectTransform.anchoredPosition;
+            _coinRectTransform.anchoredPosition = _startPosition;
+            _coinRectTransform.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
         }
 
         #endregion
@@ -102,6 +116,13 @@ namespace Interface.EndGame
             _progressBarAmountImage.DOFillAmount(1f, _timeAnimation);
 
             yield return new WaitForSeconds(_timeAnimation);
+
+            _coinRectTransform.GetComponent<Image>().DOColor(new Color(1f, 1f, 1f, 1f), _timeAnimation / 2);
+            _coinRectTransform.DOAnchorPos(_targetPosition, _timePlayingAnimation);
+
+            yield return new WaitForSeconds(_timeAnimation / 2);
+
+            _coinRectTransform.DOScale(new Vector3(0f, 0f, 0f), _timeAnimation / 2);
 
             OnGetCoins?.Invoke(_coinsValue);
 
