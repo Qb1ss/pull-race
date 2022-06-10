33383c0578ee34248094b7d15127fee1146edd21
Gleb B <gleb.bortsov79@gmail.com>
2022-06-10 12:57:10 +1000

    Добавил:
    * модель границы препятствия

--- Assets/Scripts/Configs/ChunkConfig.cs
+++ Assets/Scripts/Configs/ChunkConfig.cs
@@ -7,9 +7,13 @@ namespace Configs
     public class ChunkConfig : ScriptableObject
     {
         [Header("Chunk Parameters")]
+        [SerializeField] private float _xSpawnerPosition = 6.5f;
+        [Space(height: 5f)]
+
         [SerializeField] private Obstruction[] _obstructionsPrefabs;
 
         #region Private Fields
+        public float XSpawnerPosition => _xSpawnerPosition;
 
         public Obstruction[] ObstructionsPrefabs => _obstructionsPrefabs;
 
