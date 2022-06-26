14b7991eaad92c9083d52d453c4b51f5fa32166c
Gleb B <gleb.bortsov79@gmail.com>
2022-06-26 10:50:16 +1000

    Работа:
    * merge визуала и кода

--- Assets/Scripts/UI/End Game/WinGamePanel.cs
+++ Assets/Scripts/UI/End Game/WinGamePanel.cs
@@ -5,7 +5,6 @@ using System.Collections;
 using TMPro;
 using DG.Tweening;
 using Scenes;
-using Character;
 
 namespace Interface.EndGame
 {
@@ -24,12 +23,8 @@ namespace Interface.EndGame
         [SerializeField] private Button _nextLevelButton;
         [Space(height: 5f)]
 
-        [SerializeField] private TextMeshProUGUI _headerText;
-        [SerializeField] private TextMeshProUGUI _buttonNextLevelText;
+        [SerializeField] private TextMeshProUGUI _getCoinText;
 
-        private Image _winGamePanel;
-
-        private int _coinDivision = 10;
         private int _coinsValue;
 
         [Header("Coin Animation")]
@@ -53,7 +48,7 @@ namespace Interface.EndGame
 
             _nextLevelButton.onClick.AddListener(() => TrancitionToNextLevel());
 
-            UpdateStartVisual();
+            StartCoroutine(ProgressBarCoroutine());
 
             _targetPosition = _coinRectTransform.anchoredPosition;
             _coinRectTransform.anchoredPosition = _startPosition;
@@ -64,30 +59,6 @@ namespace Interface.EndGame
 
         #region Private Methods
 
-        private void UpdateStartVisual()
-        {
-            _winGamePanel = GetComponent<Image>();
-
-            _nextLevelButton.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
-            _headerText.color = new Color(1f, 1f, 1f, 0f);
-            _buttonNextLevelText.color = new Color(1f, 1f, 1f, 0f);
-            _winGamePanel.color = new Color(0f, 0f, 0f, 0f);
-
-            StartingAnimation();
-        }
-
-
-        private void StartingAnimation()
-        {
-            _nextLevelButton.GetComponent<Image>().DOColor(new Color(1f, 1f, 1f, 1f), _timeAnimation);
-            _headerText.DOColor(new Color(1f, 1f, 1f, 1f), _timeAnimation);
-            _buttonNextLevelText.DOColor(new Color(1f, 1f, 1f, 1f), _timeAnimation);
-            _winGamePanel.DOColor(new Color(0f, 0f, 0f, 1f), _timeAnimation);
-
-            StartCoroutine(ProgressBarCoroutine());
-        }
-
-
         private void TrancitionToNextLevel()
         {
             _sceneTrancition.OnTrancitionToNextScene();
@@ -97,6 +68,8 @@ namespace Interface.EndGame
 
         private IEnumerator ProgressBarCoroutine()
         {
+            _getCoinText.text = $"{_coinsValue}";
+
             OnGetCoins?.Invoke(_coinsValue);
 
             yield return new WaitForSeconds(_timeAnimation);
